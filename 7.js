cc2ae34d2a195f29ff40146f952b81ca4fdf4851
Victoria <151223562+FoxieFo@users.noreply.github.com>
2024-01-02 19:29:10 +0100

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,24 +1,24 @@
 // Задание 7
 
-const arrNums = [1, 2, 0, 5, 8, 0, 10, 7, 3];
+const arrNums = [1, 2, 0, 5, 8, 'orange', false, null, 0, 10, 7, 3];
 
-let evenCount = 0;
-let oddCount = 0;
-let zeroCount = 0;
+let evenNums = 0;
+let oddNums = 0;
+let zeroNums = 0;
 
-for (let i = 0; i < arrNums.length; i++) {
+for (i = 0; i < arrNums.length; i++) {
 
-  if (typeof arrNums[i] === 'number' && !isNaN(arrNums[i])) {
-    if (arrNums[i] === 0) {
-      zeroCount++;
-    } else if (arrNums[i] % 2 === 0) {
-      evenCount++;
-    } else {
-      oddCount++;
+    if (typeof arrNums[i] === 'number' && !isNaN(arrNums[i])) {
+        if (arrNums[i] === 0) {
+            zeroNums++
+        } else if (arrNums[i] % 2 === 0) {
+            evenNums++
+        } else {
+            oddNums++
+        }
     }
-  }
 }
 
-console.log('Количество четных элементов:', evenCount);
-console.log('Количество нечетных элементов:', oddCount);
-console.log('Количество нулевых элементов:', zeroCount);
+console.log('Количество четных элементов:', evenNums);
+console.log('Количество нечетных элементов:', oddNums);
+console.log('Количество нулевых элементов:', zeroNums);
